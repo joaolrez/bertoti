d08b38d5c1ff08a9ed7827b3fee1c16c2eec597d
João Pedro Lopes <102550311+joaolrez@users.noreply.github.com>
2022-11-25 05:28:14 -0300

    Add files via upload

---
+++ Engenharia 1/src/factory/ConnectionFactory.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package factory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author jprez
+ */
+public class ConnectionFactory {
+    public Connection getConnection(){
+                 try {
+                     return DriverManager.getConnection("jdbc:mysql://localhost/mercado","root","fatec123");
+                        
+                  }
+                 catch(SQLException excecao){
+                     throw new RuntimeException(excecao);
+                 }
+    }
+}
